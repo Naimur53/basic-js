0e158aa80202c4613117688730241985ad3f0467
Naimur53 <naimurrhman53@gmail.com>
2021-05-04 22:36:02 +0600

    learning basic js

---
+++ elemants/js/main.js
@@ -0,0 +1,19 @@
+console.log(4); //print some thing 
+var age = 19;
+var age2 = 23;
+var result = age+age2;
+var name ="sadi";
+console.log( "ddd");
+var iscool =true; //bolean type variable
+console.log(typeof iscool, typeof (name),typeof age); //typeof will said which type the variable is 
+var nsi_dfdfd = "kkdfk"; //sting type variable
+console.log(nsi_dfdfd);  
+var promise = "i am Naimur rahman brother JKJKlks ifdsjfkdsjfkds ";
+var low = promise.toUpperCase(); //text will trasnfrom in uppercase 
+console.log(low);
+console.log(low.indexOf("NAIMUR")); //idnexOf will show the position of it  the   in variable 
+console.log(low.split(""));//it will separet every word and space in string 
+var n1 = 44;
+var n2 =" 30.3";
+n2 =parseInt(n2); //variable convertar int type .......parseFloat
+console.log(n2);
